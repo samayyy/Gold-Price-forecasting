ce49781df2525876a3ccd4b374c9b576c28d9a78
samayyy <66873003+samayyy@users.noreply.github.com>
2023-03-09 11:51:22 +0530

    Update out.py

--- out.py
+++ out.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
-%matplotlib inline
+# %matplotlib inline
 import seaborn as sns
 import warnings
 warnings.filterwarnings('ignore')
